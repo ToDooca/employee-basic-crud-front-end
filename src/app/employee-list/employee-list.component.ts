79c07a1b6c98c9aa4acefc70fff1853597721c2d
Dušan Stanković <todooc4@gmail.com>
2020-12-21 00:48:40 +0100

    created delete methods

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import  { Employee} from "../employee";
 import {EmployeeService} from "../employee.service";
 import {Router} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-employee-list',
@@ -26,6 +27,13 @@ export class EmployeeListComponent implements OnInit {
 
   updateEmployee(id: number){
     this.router.navigate(['update-employee', id]);
+  }
+
+  deleteEmployee(id:number){
+    this.employeeService.deleteEmployee(id).subscribe(data =>{
+      console.log(data);
+      this.getEmployees();
+    });
   }
 
 }
