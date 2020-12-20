05c7c7ac1efaa2dc0863cf8ff3f1039ff2566016
Dušan Stanković <todooc4@gmail.com>
2020-12-21 00:01:50 +0100

    created uptade employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 import { FormsModule } from "@angular/forms";
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
